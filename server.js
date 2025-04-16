90afc23b5cdc5e3d6e51caa49e5feb39f588df34
Hugo Santos <bicalho.hugo@gmail.com>
2025-04-15 21:08:08 -0300

    WIP: refactor nas rotas do projeto web

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const port = 8080;
 
 app.use(express.static('public'));
 
+app.get("/users", (req, res) => { 
+    res.redirect('/users.html');
+});
+
 app.listen(port, () => {
     console.log(`Servidor rodando em http://localhost:${port}`);
 });
